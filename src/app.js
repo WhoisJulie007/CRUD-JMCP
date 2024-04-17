01fcd0c922ba5c45de69a629a825e04f6a6890b7
julia <maydeli.castan@iest.edu.mx>
2024-04-17 00:48:06 -0600

    Este es un commit de todos los archivos

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const mysql = require("mysql");
+const myConnection = require("express-myconnection");
+
+const app = express();
+
+//importing routes
+const customerRoutes = require("./routes/customer");
+
+//settings
+app.set("port", process.env.PORT || 3306);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//middlewares
+app.use(morgan("dev"));
+app.use(
+  myConnection(
+    mysql,
+    {
+      host: "db5007094691.hosting-data.io",
+      user: "dbu2427698",
+      password: "iestdev0!",
+      database: "dbs5848722",
+    },
+    "single"
+  )
+);
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/", customerRoutes);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${process.env.PORT || 3000}`);
+});
